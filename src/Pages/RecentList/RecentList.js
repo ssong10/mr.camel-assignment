9615c469112bee976d38ff9bb096a0de6a046574
ssong10 <leesy1403@naver.com>
2021-07-30 19:07:32 +0900

    Feat: RecentList/sortModal - 정렬 모달 구현

--- src/Pages/RecentList/RecentList.js
+++ src/Pages/RecentList/RecentList.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import RecentCard from './RecentCard';
+import INITIALDATA from '../../asset/data.json'
+import Button from '../../Components/button'
+import SortModal from './sortModal'
 
 class RecentList extends React.Component{
+    state = {
+        defaultItems : INITIALDATA,
+        recentList : [{id:1,time:4},{id:2,time:12},{id:4,time:20},{id:20,time:3}],
+        modal:false
+    }
+    toggleModal = () => {
+        this.setState({modal:!this.state.modal})
+    }
+    sortByPrice = () => {
+        const {defaultItems,recentList} = this.state
+        return recentList.sort((a,b) => {
+            return (defaultItems[a.id].price - defaultItems[b.id].price)
+        })
+    }
+    sortByRecent = () => {
+        return this.state.recentList.sort((a,b) => {
+            return a.time - b.time
+        })
+    }
+    sortItem = (type) => {
+        let sortedItem;
+        if (type === 'price') {
+            sortedItem =  this.sortByPrice()
+        }
+        if (type === 'recent') {
+            sortedItem = this.sortByRecent()
+        }
+        this.setState({recentList:sortedItem})
+        console.log(sortedItem)
+    }
     render() {
         return (
             <div>
-                <RecentCard />
-                <Link to='/'>홈</Link>
+                <RecentCard
+                    defaultItems={this.state.defaultItems}
+
+                />
+                <Button onClick={this.toggleModal}>정렬</Button>
+                <div>
+                <SortModal 
+                    show={this.state.modal}
+                    toggle={this.toggleModal}
+                    sortItem={this.sortItem}
+                >
+                </SortModal>
+                </div>
             </div>
         )
     }
