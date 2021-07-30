ba4919f55e487fe25d71645d9b7a0faa4137ea4e
yuno <hanganda23@naver.com>
2021-07-30 15:30:42 +0900

    feat : price 쉼표 포맷

---
+++ src/utils/format.js
@@ -0,0 +1,22 @@
+export function moneyFormat(price) {
+    if (isNaN(Number(price))) { 
+        return NaN;
+    }
+
+    let result = '';
+    let count = 0;
+
+    while (price > 0) {
+        const num = price % 10;
+        price = Math.floor(price/10);
+        result = String(num) + result;
+        count++;
+
+        if(count === 3 && price > 0) {
+            count = 0;
+            result = ',' + result;
+        }
+    }
+
+    return result;
+}
