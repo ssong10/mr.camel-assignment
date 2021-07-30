64bb079db038b34e148f968cd2178ac06521290d
ssong10 <leesy1403@naver.com>
2021-07-30 12:47:15 +0900

    Feat: 헤더, 버튼 공통컴포넌트 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Routes from 'Routes/Routes';
-
+import Header from 'components/header'
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes />
     </div>
   );
