5e0fb1195f6ffe53a5f220e35cf18d297cc4834d
yuno <hanganda23@naver.com>
2021-07-31 16:25:00 +0900

    feat : products data use fetch

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -3,87 +3,14 @@ import styled from 'styled-components';
 import Button from 'Components/button';
 import { moneyFormat } from 'utils/format';
 import { unInterestLocalStorage, recentShowLocalStorage } from 'utils/localStorage';
-import products from './product.json';
-
-class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = this.props.location.state ?? {};
-  }
-
-  componentDidMount() {
-    const { history } = this.props;
-    const { id } = this.state;
-
-    if (id === undefined || unInterestLocalStorage.includes(this.state.id)) {
-      history.goBack();
-      return;
-    }
-
-    recentShowLocalStorage.push({ id, time: Date.now() });
-  }
-
-  showRandomProduct = () => {
-    const { history } = this.props;
-
-    const unInterestIds = unInterestLocalStorage.list.map(item => item.id);
-    const filterProduct = products.filter((item, id) => !unInterestIds.includes(id)); // 임시 : id 수정 필
-
-    if (filterProduct.length === 0) {
-      alert('더 이상 없습니다.');
-      history.push('/');
-    }
-
-    const randomIndex = Math.floor(Math.random() * filterProduct.length);
-    const nextProduct = filterProduct[randomIndex];
-
-    recentShowLocalStorage.push({ id: randomIndex, time: Date.now() }); // 임시 : id 수정 필
-
-    this.setState({
-      id: randomIndex, ...nextProduct // 임시 : id 수정 필
-    });
-  }
-
-  addUnInterest = () => {
-    const { id } = this.state;
-
-    unInterestLocalStorage.push({ id, time: Date.now() });
-
-    this.showRandomProduct();
-  }
-
-  render() {
-    const { id, title, brand, price } = this.state;
-
-    if (id === undefined) return null;
-
-    return (
-      <Container>
-        <Title>{title}</Title>
-        <Brand>{brand}</Brand>
-        <Row>
-          <span>판매가 : </span>
-          <div>
-            <Price>{moneyFormat(price)}</Price><span>원</span>
-          </div>
-        </Row>
-        <Row>
-          <Button onClick={this.showRandomProduct}>랜덤 상품 보기</Button>
-          <Button onClick={this.addUnInterest}>관심 없음</Button>
-        </Row>
-
-        {/* <Link to='/recentList'>최근조회이력</Link> */}
-      </Container>
-    )
-  }
-}
+import { fetchProduct } from 'utils/api';
 
 const Container = styled.div`
     display : flex;
     flex-direction: column;
     max-width : 500px;
     margin : auto;
+    padding : 30px 0;
 `;
 const Row = styled.div`
     display : flex;
@@ -105,5 +32,83 @@ const Price = styled.span`
     color : rgb(240, 40, 40);
     font-weight : 600;
 `;
+
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { item : this.props.location.state }
+  }
+
+  componentDidMount() {
+    const { history } = this.props;
+    const { id } = this.state.item;
+
+    if (!this.state.item || unInterestLocalStorage.includes(id)) {
+      history.goBack();
+      return;
+    }
+
+    recentShowLocalStorage.push({ id, time: Date.now() });
+    this.getProducts();
+  }
+
+  async getProducts() {
+    this.products = await fetchProduct();
+  }
+
+  addUnInterest = () => {
+    const { id } = this.state.item;
+
+    unInterestLocalStorage.push({ id, time: Date.now() });
+
+    this.showRandomProduct();
+  }
+
+  showRandomProduct = () => {
+    const { history } = this.props;
+
+    const filterProduct = this.products.filter((item) => !unInterestLocalStorage.includes(item.id));
+    console.log(filterProduct.length);
+
+    if (filterProduct.length === 0) {
+      alert('더 이상 없습니다.');
+      history.push('/');
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * filterProduct.length);
+    const nextProduct = filterProduct[randomIndex];
+
+    recentShowLocalStorage.push({ id: nextProduct.id, time: Date.now() });
+
+    this.setState({
+      item : nextProduct
+    });
+  }
+
+  render() {
+    if (!this.state.item) return null;
+
+    const { title, brand, price } = this.state.item;
+
+    return (
+      <Container>
+        <Title>{title}</Title>
+        <Brand>{brand}</Brand>
+        <Row>
+          <span>판매가 : </span>
+          <div>
+            <Price>{moneyFormat(price)}</Price><span>원</span>
+          </div>
+        </Row>
+        <Row>
+          <Button onClick={this.showRandomProduct}>랜덤 상품 보기</Button>
+          <Button onClick={this.addUnInterest}>관심 없음</Button>
+        </Row>
+      </Container>
+    )
+  }
+}
 
 export default Product;
