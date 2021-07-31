b4e2b31aa8ddd5285a5ebe64d7e547e9f4df8b7b
KANGDONGHWI <rkdehdgnl@naver.com>
2021-07-31 17:39:45 +0900

    Feat Product로 링크

--- src/Pages/RecentList/RecentCard.js
+++ src/Pages/RecentList/RecentCard.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import {Link} from 'react-router-dom';
 
 const CardContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin-top:30px;
 `;
-const Card = styled.div`
-    border : 0.5px solid black;
-    display: flex;
-    flex-direction: column;
-    margin-left: calc((100% - (20% * 4)) / 4);
-    margin-bottom:10px;
-    width: 250px;
-    height: 250px;
+const Card = styled(Link)`
+  border : 0.5px solid black;
+  display: flex;
+  flex-direction: column;
+  margin-left: calc((100% - (20% * 4)) / 4);
+  margin-bottom:20px;
+  text-align: center;
 `;
+const Title = styled.div`
+  font-size:16px;
+  font-weight: bold;
+  text-align: center;
+  padding:20px;
+`;
+const Price = styled.div`
+   text-align: right;
+`;
+const Brand = styled.div`
+  padding-top:5px;
+  font-weight: bold;
+  
+`;
+
+
 class RecentCard extends Component {
   render() {
     const { showItem } = this.props
+
     return (
       <>
         <CardContainer>
           {showItem.map((c,index) => (
-            <Card key={index}>
-              <div>{c.title}</div>
-              <div>{c.price}원</div>
-              <div>{c.brand}</div>
+            <Card key={index} to={{
+              pathname: '/product',
+            }}>
+              <Brand>{c.brand}</Brand>
+              <Title>{c.title}</Title>
+              <Price>{c.price}원</Price>
               <div>id:{c.id}</div>
               <div>time:{c.time}</div>
             </Card>
